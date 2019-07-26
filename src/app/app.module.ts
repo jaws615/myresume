d8c95e553f75a09a93001dfc04d336ad5cfbbc8b
jaws615 <49572576+jaws615@users.noreply.github.com>
2019-07-26 01:13:18 -0400

    creating resume

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
+
 import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
@@ -26,7 +29,8 @@ import { LayoutModule } from '@angular/cdk/layout';
     MatMenuModule,
     MatIconModule,
     MatButtonModule,
-    LayoutModule
+    LayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/show-me/show-me.component.ts
+++ src/app/show-me/show-me.component.ts
@@ -13,18 +13,21 @@ export class ShowMeComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'About', cols: 1, rows: 1, content: '' },
+          { title: 'Card 2', cols: 1, rows: 1, content: '' },
+          { title: 'Card 3', cols: 1, rows: 1, content: '' },
+          { title: 'Card 4', cols: 1, rows: 1, content: '' }
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { id: 1, title: 'About', cols: 2, rows: 1, content: 'Dedicated Software Development Director with over 18 years of progressive information technology experience, 10 of them in managerial positions. Background includes leading large/small teams of technical professionals, creating and improving project management strategies, cultivating customer relationships and improving team and project performance. Strong communicator, able to find the proper communication approach to executive or subordinate. ' },
+        { id: 2, title: '', cols: 1, rows: 2, content: ['Large/small IT team management',
+          'Software product development and promotion',
+          'Project Management'
+          ]},
+        { id: 3, title: '', cols: 1, rows: 2, content: '' },
+        { id: 4, title: 'Card 4', cols: 1, rows: 2, content: '' }
       ];
     })
   );
