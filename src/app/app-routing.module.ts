becdbede83e44127665da41b8cc6629b109666b0
jaws615 <49572576+jaws615@users.noreply.github.com>
2019-08-01 11:43:46 -0400

    content with panels

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ShowMeComponent } from './show-me/show-me.component';
+import { ExpansionPanelComponent } from './expansion-panel/expansion-panel.component';
 
 
 const routes: Routes = [
-  { path: 'resume', component: ShowMeComponent }
+  { path: 'resume', component: ShowMeComponent },
+  { path: 'resumePanel', component: ExpansionPanelComponent }
 ];
 
 @NgModule({
