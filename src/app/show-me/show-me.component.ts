15cf6f8c3bae418c92f740a7dd2292b1d6605c06
jaws615 <49572576+jaws615@users.noreply.github.com>
2019-08-01 10:07:19 -0400

    adding theme

--- src/app/show-me/show-me.component.ts
+++ src/app/show-me/show-me.component.ts
@@ -16,15 +16,29 @@ export class ShowMeComponent {
           { title: 'Card 1', cols: 1, rows: 1 },
           { title: 'Card 2', cols: 1, rows: 1 },
           { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'Card 4', cols: 1, rows: 1 },
+          { title: 'Card 6', cols: 1, rows: 1 },
+          { title: 'Card 7', cols: 1, rows: 1 },
+          { title: 'Card 8', cols: 1, rows: 1 },
+          { title: 'Card 9', cols: 1, rows: 1 },
+          { title: 'Card 10', cols: 1, rows: 1 },
+          { title: 'Card 11', cols: 1, rows: 1 },
+          { title: 'Card 11', cols: 2, rows: 1 }
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
+        { title: 'Card 1', cols: 1, rows: 1 },
         { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: 'Card 3', cols: 1, rows: 1 },
+        { title: 'Card 4', cols: 1, rows: 1 },
+        { title: 'Card 6', cols: 1, rows: 1 },
+        { title: 'Card 7', cols: 1, rows: 1 },
+        { title: 'Card 8', cols: 1, rows: 1 },
+        { title: 'Card 9', cols: 1, rows: 1 },
+        { title: 'Card 10', cols: 1, rows: 1 },
+        { title: 'Card 11', cols: 1, rows: 1 },
+        { title: 'Card 11', cols: 2, rows: 1 }
       ];
     })
   );
